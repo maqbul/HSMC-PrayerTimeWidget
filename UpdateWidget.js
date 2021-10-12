68abc79fdcdd2e7421cceb5f06648bbbb324421e
MIS Productions <maqbul.yusuf@sky.com>
2021-10-12 12:01:00 +0100

    Create UpdateWidget.js
    Auto updates, works offline using an available module

---
+++ UpdateWidget.js
@@ -0,0 +1,12 @@
+// jshint -W119
+
+async function main() {
+	let widget = new ListWidget();
+  widget.addText('Hello duniya');
+ 	let value = (config.runsInWidget) ? Script.setWidget(widget) : await widget.presentMedium();
+ 	Script.complete();
+}
+
+module.exports = {
+  main
+};
